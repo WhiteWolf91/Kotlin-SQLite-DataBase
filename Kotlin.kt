5d51e1e0805f382ed1fdc219942968663ace02de
White Wolf <121308209+WhiteWolf91@users.noreply.github.com>
2023-08-06 10:13:15 +0300

    Kotlin.kt

--- Kotlin.kt
+++ Kotlin.kt
@@ -1,14 +1,19 @@
 try {
 
-            val myDataBase = this.openOrCreateDatabase("Muzisyen", MODE_PRIVATE,null) // Burda Data Base Oluşuyor Muzisyen Diye
-            myDataBase.execSQL("CREATE TABLE IF NOT EXISTS muzisyenn (id INTEGER PRIMARY KEY,name VARCHAR,age INT)") // Burda muzisyenn Tablosu Açılıyor
+            val myDataBase = this.openOrCreateDatabase("Muzisyen", MODE_PRIVATE,null)
+            myDataBase.execSQL("CREATE TABLE IF NOT EXISTS muzisyenn (id INTEGER PRIMARY KEY,name VARCHAR,age INT)")
 
-            /*myDataBase.execSQL("INSERT INTO muzisyenn (name,age) VALUES('Barış Manco',56)") // Burda Sanatcılar Ekleniyor
-            myDataBase.execSQL("INSERT INTO muzisyenn (name,age) VALUES('Neşet Ertaş',74)")
-            myDataBase.execSQL("INSERT INTO muzisyenn (name,age) VALUES('Aşık Veysel',78)")
+
+            /*myDataBase.execSQL("INSERT INTO muzisyenn (name,age) VALUES('Barış Manco',56)") //İd = 1
+            myDataBase.execSQL("INSERT INTO muzisyenn (name,age) VALUES('Aşık Veysel',78)") //İd = 2
+            myDataBase.execSQL("INSERT INTO muzisyenn (name,age) VALUES('Neşet Ertaş',74)") //İd = 3
+
              */
 
-            val cursor = myDataBase.rawQuery("SELECT * FROM muzisyenn WHERE name LIKE 'Aş%el'",null) // Burda Aş İle BAşlayıp el İle Biten SAnatcı İsmi Arıyor
+            myDataBase.execSQL("UPDATE muzisyenn SET age = 78 WHERE id = 2")
+            // myDataBase.execSQL("DELETE FROM muzisyenn WHERE name = 'Neşet Ertaş'")
+
+            val cursor = myDataBase.rawQuery("SELECT * FROM muzisyenn WHERE name LIKE 'Ne%aş'",null)
             val name = cursor.getColumnIndex("name")
             val age = cursor.getColumnIndex("age")
             val id = cursor.getColumnIndex("id")
